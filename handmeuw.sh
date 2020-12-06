ef4db20f027083a43c54e4a4e39c18b4c5df1017
R.F <ramonfallon@gmail.com>
2020-12-06 13:43:26 +0000

    moved over to uwsgi readthedocs documentation. handme will srver a pythoin func to a client browser now. uwsgiparams just copied in

--- handmeuw.sh
+++ handmeuw.sh
@@ -2,4 +2,4 @@
 # run a bare pythin function using uwsgi.
 # this will serve simple string on port 8000
 # and note, no web server is being used!
-uwsgi --http :8000 --uwsgi-file $1
+uwsgi --http :8000 --wsgi-file $1
